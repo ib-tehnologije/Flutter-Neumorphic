f16b93f80d678c7fa975444262244a96fa9f7912
Igor Benić <igbenic@icloud.com>
2024-02-10 11:52:10 +0100

    input decoration for neudropdown

--- lib/src/widget/neu_dropdown.dart
+++ lib/src/widget/neu_dropdown.dart
@@ -6,6 +6,8 @@ class NeuDropdown<T> extends StatelessWidget {
   final List<DropdownMenuItem<T>> items;
   final ValueChanged<T?> onChanged;
   final T? value;
+  final String placeholder;
+  final String? label;
 
   // Add any other properties you need
 
@@ -13,23 +15,34 @@ class NeuDropdown<T> extends StatelessWidget {
     Key? key,
     required this.items,
     required this.onChanged,
+    this.label,
+    this.placeholder = "Odaberite",
     this.value, // Add any other parameters for customization
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Neu(
-      style: GlobalNeuSettings.neumorphicStyle.copyWith(
-        color: Colors.white,
-      ),
-      child: DropdownButton<T>(
-        value: value,
-        underline: Container(),
-        borderRadius: BorderRadius.circular(10),
-        items: items,
-        onChanged: onChanged, // Make sure to pass the onChanged callback
-        // Add any other DropdownButton customizations here
-      ),
-    );
+    return InputDecorator(
+        decoration: InputDecoration(
+          labelText: label,
+          border: OutlineInputBorder(),
+          contentPadding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+        ),
+        child: Neu(
+          style: GlobalNeuSettings.neumorphicStyle.copyWith(
+            color: Colors.white,
+          ),
+          child: DropdownButtonHideUnderline(
+            child: DropdownButton<T>(
+              value: value,
+              borderRadius: BorderRadius.circular(10),
+              hint: value == null ? Text(placeholder) : null,
+              items: items,
+              isExpanded: true,
+              onChanged: onChanged, // Make sure to pass the onChanged callback
+              // Add any other DropdownButton customizations here
+            ),
+          ),
+        ));
   }
 }
